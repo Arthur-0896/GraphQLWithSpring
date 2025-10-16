8fa5fe723e67f3105aed2b12ab98915bc4240231
Arthur <arthur.tristram1@gmail.com>
2025-10-16 13:18:49 -0400

    Made CORS property configurable

--- GraphQLWithSpring/src/main/java/com/gqlws/WebConfig.java
+++ GraphQLWithSpring/src/main/java/com/gqlws/WebConfig.java
@@ -1,5 +1,6 @@
 package com.gqlws;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+	@Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+	
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/graphql")   // Whitelist your GraphQL endpoint
-                        .allowedOrigins("http://localhost:3000") // Allow Altair & your frontend
+                        .allowedOrigins(allowedOrigins) // Allow Altair & your frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
