13431e8463c4278bc54d282270cf9a8ac5d04fef
Arthur <arthur.tristram1@gmail.com>
2025-10-16 12:37:26 -0400

    Added CORS policy and updated schema and data

---
+++ GraphQLWithSpring/src/main/java/com/gqlws/WebConfig.java
@@ -0,0 +1,24 @@
+package com.gqlws;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/graphql")   // Whitelist your GraphQL endpoint
+                        .allowedOrigins("http://localhost:3000") // Allow Altair & your frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
+
